1750fbaed26fbe03bdf883ab58d101c6a34c2f1c
Alejandro956 <alex.torres1000@outlook.com>
2025-06-27 11:42:05 -0600

    Add files via upload

---
+++ actividad_pywhatkit.py
@@ -0,0 +1,10 @@
+import pywhatkit as pwk
+pwk.search("Que es la programacion orientada a objetos")
+
+
+pwk.playonyt("Python programacion para principiantes")
+
+
+pwk.text_to_handwriting("Hola, esto es una prueba con pywhatkit.", rgb =[0, 0, 200])
+
+print("!Acciones completadas¡")
